53cf90ca80c4bdbe34b34de5bad262368ec3c7ea
gleb <glebgi23@mail.ru>
2022-05-05 23:11:03 +0300

    normal parsing added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import requests, json
+import requests
+from tqdm import tqdm
 
-url = "http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=STEAMKEY&format=json"
-game_info_url = "http://store.steampowered.com/api/appdetails?appids="
+all_games_url = "https://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=STEAMKEY&format=json"
+game_info_url = "https://store.steampowered.com/api/appdetails?appids="
 
 
 def get_games(url):
@@ -9,50 +10,62 @@ def get_games(url):
     return games
 
 
-def get_games_info(game_info_url):
-    games = get_games(url)
+def get_games_info(url):
+    games = get_games(all_games_url)
     games = list(filter(lambda x: x["name"], games))
 
-    for game in games:
+    data = []
 
-        game_info_answer = requests.get(game_info_url + str(game["appid"])).json()
+    for game in tqdm(games):
 
-        if game_info_answer:
+        game_info_answer = requests.get(url + str(game["appid"])).json()
 
-            if game_info_answer[str(game["appid"])]["success"]:
+        if game_info_answer and game_info_answer[str(game["appid"])]["success"]:
 
-                if game_info_answer[str(game["appid"])]["data"]["type"] == "game":
+            if game_info_answer[str(game["appid"])]["data"]["type"] == "game":
 
-                    if game_info_answer[str(game["appid"])]["data"]["is_free"] is False:
-                        print(game_info_answer)
+                if not game_info_answer[str(game["appid"])]["data"]["is_free"]:
 
+                    if not game_info_answer[str(game["appid"])]["data"]["release_date"]["coming_soon"]:
 
-# for game in games:
-#
-#
-#
-#         game_info_answer = requests.get(game_info_url + str(game["appid"])).json()
-#
-#         if game_info_answer:
-#             game_info_answer = game_info_answer[str(game["appid"])]
-#
-#             if game_info_answer["success"]:
-#                 game_info_answer = game_info_answer["data"]
-#
-#                 if game_info_answer["is_free"] is False:
-#
-#                     if game_info_answer["release_date"]["coming_soon"] is False:
-#                         print(str(game["appid"]))
-#                         if game_info_answer["price_overview"]["discount_percent"] != 0:
-#                             print(str(game["appid"]))
-#                             name = game_info_answer["name"]
-#                             print(name)
-#                             print(game_info_answer["price_overview"])
-#                             if game_info_answer["genres"]:
-#                                 print(game_info_answer["genres"])
-#                             print()
-#
-#
+                        if game_info_answer[str(game["appid"])]["data"].get("price_overview"):
+
+                            if game_info_answer[str(game["appid"])]["data"]["price_overview"]["discount_percent"] != 0:
+
+                                if game_info_answer[str(game["appid"])]["data"].get("genres"):
+                                    genres = [i["description"] for i in
+                                              game_info_answer[str(game["appid"])]["data"]["genres"]]
+                                else:
+                                    genres = []
+
+                                if game_info_answer[str(game["appid"])]["data"].get("screenshots"):
+                                    screenshots = [i["path_thumbnail"] for i in
+                                                   game_info_answer[str(game["appid"])]["data"]["screenshots"]]
+                                else:
+                                    screenshots = []
+
+                                name = game_info_answer[str(game["appid"])]["data"]["name"]
+                                short_description = game_info_answer[str(game["appid"])]["data"][
+                                    "short_description"]
+                                discount = game_info_answer[str(game["appid"])]["data"]["price_overview"][
+                                    "discount_percent"]
+                                base_price = game_info_answer[str(game["appid"])]["data"]["price_overview"][
+                                    "initial_formatted"]
+                                current_price = game_info_answer[str(game["appid"])]["data"]["price_overview"][
+                                    "final_formatted"]
+                                platforms = game_info_answer[str(game["appid"])]["data"]["platforms"]
+
+                                data.append({
+                                    "name": name,
+                                    "short_description": short_description,
+                                    "discount": discount,
+                                    "base_price": base_price,
+                                    "current_price": current_price,
+                                    "platforms": platforms,
+                                    "screenshots": screenshots,
+                                    "genres": genres
+                                })
+
 
 if __name__ == "__main__":
     get_games_info(game_info_url)
