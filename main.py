50fbdfd5ec36a36aef066dd3f5dd7868b86df3fc
gleb <glebgi23@mail.ru>
2022-05-06 19:16:53 +0300

    new parsing added

--- main.py
+++ main.py
@@ -5,26 +5,27 @@ import sqlite3
 
 last_game = 0
 
-salepage_url = f"https://store.steampowered.com/search/results/?query&start={last_game}" \
-               f"&count=50&dynamic_data=&sort_by=_ASC&snr=1_7_7_2300_7&specials=1&infinite=1"
+salepage_url = "https://store.steampowered.com/search/results/?query&start={0}" \
+               "&count=50&dynamic_data=&sort_by=_ASC&snr=1_7_7_2300_7&specials=1&infinite=1"
 
 game_info_url = "https://store.steampowered.com/api/appdetails?appids="
 
 
 def parse_salepage(url):
-    global last_game
 
     games = []
 
-    answer = requests.get(url).json()
-    soup = BeautifulSoup(answer["results_html"], 'html.parser')
-    links = soup.find_all("a")
-    for _ in range(answer["total_count"] // 50):
+    answer = requests.get(url.format(last_game)).json()
+    for offset in tqdm(range(0, answer["total_count"], 50)):
+        answer = requests.get(url.format(offset)).json()
+        soup = BeautifulSoup(answer["results_html"], 'html.parser')
+        links = soup.find_all("a")
+
         for i in links:
             appid = i.attrs.get("data-ds-appid")
             if appid:
-                games.append(appid)
-
+                if len(appid.split(",")) == 1:
+                    games.append(appid)
     return games
 
 
@@ -33,44 +34,37 @@ def get_info_about_games(url):
 
     for appid in tqdm(parse_salepage(salepage_url)):
         answer = requests.get(url + appid).json()
-        # print(str(appid))
         if answer:
             answer = answer[str(appid)]
-            games.append(1)
+            if answer["success"] and answer["data"]["type"] == "game" and not answer["data"]["is_free"]:
+                name = answer["data"]["name"]
+                short_description = answer["data"]["short_description"]
+                discount_percent = answer["data"]["price_overview"]["discount_percent"]
+                initial_price = answer["data"]["price_overview"]["initial_formatted"]
+                final_formatted = answer["data"]["price_overview"]["final_formatted"]
+                platforms = answer["data"]["platforms"]
+                genres = [genre["description"] for genre in answer["data"]["genres"]]
+                screenshots = [screenshot["path_thumbnail"] for screenshot in answer["data"]["screenshots"]]
+                game_url = f"https://store.steampowered.com/app/{appid}"
+                games.append({
+                    "name": name,
+                    "short_description": short_description,
+                    "discount_percent": discount_percent,
+                    "initial_price": initial_price,
+                    "final_formatted": final_formatted,
+                    "platforms": platforms,
+                    "genres": genres,
+                    "screenshots": screenshots,
+                    "game_url": game_url,
+                    "appid": appid
+                })
 
-        if len(games) == 10:
-            return games
-        # print(answer)
-        # print()
-        # if answer["success"] and answer["data"]["type"] == "game" and not answer["data"]["is_free"]:
-        #     name = answer["data"]["name"]
-        #     short_description = answer["data"]["short_description"]
-        #     discount_percent = answer["data"]["price_overview"]["discount_percent"]
-        #     initial_price = answer["data"]["price_overview"]["initial_formatted"]
-        #     final_formatted = answer["data"]["price_overview"]["final_formatted"]
-        #     platforms = answer["data"]["platforms"]
-        #     genres = [genre["description"] for genre in answer["data"]["genres"]]
-        #     screenshots = [screenshot["path_thumbnail"] for screenshot in answer["data"]["screenshots"]]
-        #     game_url = f"https://store.steampowered.com/app/{appid}"
-        #     games.append({
-        #         "name": name,
-        #         "short_description": short_description,
-        #         "discount_percent": discount_percent,
-        #         "initial_price": initial_price,
-        #         "final_formatted": final_formatted,
-        #         "platforms": platforms,
-        #         "genres": genres,
-        #         "screenshots": screenshots,
-        #         "game_url": game_url,
-        #         "appid": appid
-        #     })
-
-    return len(games)
+    return games
 
 
 def write_to_database():
     connection = sqlite3.connect("steam.db")
-    for game in get_info_about_games(game_info_url):
+    for game in tqdm(get_info_about_games(game_info_url)):
         connection.execute(
             "INSERT INTO games ('appid', 'name', 'short_description', 'discount_percent', 'initial_price', 'final_formatted', 'windows', 'mac', 'linux', 'genres', 'screenshots', 'game_url') VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
             [game["appid"], game["name"], game["short_description"], game["discount_percent"], game["initial_price"],
@@ -82,4 +76,4 @@ def write_to_database():
 
 
 if __name__ == "__main__":
-    print(get_info_about_games(game_info_url))
+    write_to_database()
